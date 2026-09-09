9214699db5555b477e4cc6e2ba2c552513365df4
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2026-09-09 15:32:47 +0530

    react, tailwind basic setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-3xl font-bold underline text-pink-500">
+        Tailwind CSS is Working!
+      </h1>
+    </div>
+  )
+}
+
